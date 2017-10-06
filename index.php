c5fe79dc2dd128cb6a5adb845dbdc26d1c403e7d
nicolas <nicolas@Nicolas>
2017-10-06 00:12:10 -0500

    proyecto de procesador

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            
+        </title>
+    </head>
+    <body>
+        <form action ="procesador.php"
+              <input type ="text" name="longitud">
+             <input type ="text" name="latitud">
+             <input type ="submit">
+    </body>
+</html>
